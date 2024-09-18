139e5991ba5eaa071e9c22dffcff4c839a0c7804
Seerat Awan <seeratsdsking@gmail.com>
2024-09-18 16:05:00 +0500

    feat: updated initial config

--- packages/javascript-sdk/src/core/config.ts
+++ packages/javascript-sdk/src/core/config.ts
@@ -1,4 +1,5 @@
 import { LogLevel } from '../utils/logger';
+import { Policy } from './types';
 
 export interface Config {
     apiKey: string;
@@ -9,9 +10,29 @@ export interface Config {
     useBeaconApi?: boolean;
     forceUseFetch?: boolean;
     autocapture?: boolean;
-    formTracking?: boolean;
+    formTracking?: boolean | 'all' | 'tagged' | 'none';
     autoPageview?: boolean;
     disableEventPersistence?: boolean;
+    gaHook?: boolean;
+    segmentHook?: boolean;
+    randomizeUrl?: boolean;
+    capture3rdPartyCookies?: string[] | false;
+    idMethod?: 'cookie' | 'localStorage';
+    privacyPolicy?: 'strict';
+    ipPolicy?: Policy;
+    cookiePolicy?: Policy;
+    customHeaders?: Record<string, string> | (() => Record<string, string>);
+    minSendTimeout?: number;
+    maxSendTimeout?: number;
+    maxSendAttempts?: number;
+    propertiesStringMaxLength?: number | null;
+    propertyBlacklist?: string[];
+    exclude?: string;
+    namespace?: string;
+    crossDomainLinking?: boolean;
+    domains?: string;
+    compatMode?: boolean;
+    fetch?: any;
 }
 
 export const defaultConfig: Partial<Config> = {
@@ -22,4 +43,18 @@ export const defaultConfig: Partial<Config> = {
     formTracking: false,
     autoPageview: false,
     disableEventPersistence: false,
+    gaHook: false,
+    segmentHook: false,
+    randomizeUrl: false,
+    capture3rdPartyCookies: ['_ga', '_fbp', '_ym_uid', 'ajs_user_id', 'ajs_anonymous_id'],
+    idMethod: 'cookie',
+    ipPolicy: 'keep',
+    cookiePolicy: 'keep',
+    minSendTimeout: 0,
+    maxSendTimeout: 2000,
+    maxSendAttempts: 4,
+    propertiesStringMaxLength: null,
+    propertyBlacklist: [],
+    crossDomainLinking: true,
+    compatMode: false,
 };
--- packages/javascript-sdk/src/core/types.ts
+++ packages/javascript-sdk/src/core/types.ts
@@ -19,4 +19,4 @@ export interface UserProps {
 }
 
 
-
+export type Policy = 'strict' | 'keep' | 'comply';
